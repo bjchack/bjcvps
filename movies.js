f31a0974da357c95260deb5d23773e4f38a549ac
bjchack <bjchack@gmail.com>
2025-12-08 22:54:43 +0800

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,13 +1,14 @@
-// movies.js
 const movies = [
   {
     "title": "Bad Actor (2024)",
-    "img": "https://image.tmdb.org/t/p/original/pQZ1K2rIDqXS3.jpg",
-    "url": "http://103.145.232.246/Data/movies/Hollywood/2024/Bad%20Actor%20A%20Hollywood%20Ponzi%20Scheme%20(2024)/Bad.Actor.A.Hollywood.Ponzi.Scheme.2024.720p.WEBRip.x264.AAC-[YTS.MX].mp4"
+    "img": "https://image.tmdb.org/t/p/original/pQZ1K2rI",
+    "url": "http://188.210.209.2/EDI/Movies/Fast%20&%20Furious%209%20-%202021.mp4",
+    "category": "Action"
   },
   {
     "title": "New Movie 2025",
     "img": "https://image.tmdb.org/t/p/original/NEW_POSTER.jpg",
-    "url": "http://103.145.232.246/.../NewMovie2025.mp4"
+    "url": "http://103.145.232.246/.../NewMovie2025.mp4",
+    "category": "Sci-Fi"
   }
 ];
