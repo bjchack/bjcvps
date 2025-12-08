b8e980fbfd819c9af60c39013b7cf708059538b9
bjchack <bjchack@gmail.com>
2025-12-08 22:35:22 +0800

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,13 @@
+// movies.js
+const movies = [
+  {
+    "title": "Bad Actor (2024)",
+    "img": "https://image.tmdb.org/t/p/original/pQZ1K2rIDqXS3.jpg",
+    "url": "http://103.145.232.246/Data/movies/Hollywood/2024/Bad%20Actor%20A%20Hollywood%20Ponzi%20Scheme%20(2024)/Bad.Actor.A.Hollywood.Ponzi.Scheme.2024.720p.WEBRip.x264.AAC-[YTS.MX].mp4"
+  },
+  {
+    "title": "New Movie 2025",
+    "img": "https://image.tmdb.org/t/p/original/NEW_POSTER.jpg",
+    "url": "http://103.145.232.246/.../NewMovie2025.mp4"
+  }
+];
