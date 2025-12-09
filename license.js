00b4307aee4f1e352ce5c14f308287239cceafb0
bjchack <bjchack@gmail.com>
2025-12-09 18:28:20 +0800

    Update license.js

--- license.js
+++ license.js
@@ -1,6 +1,5 @@
 // license.js
 const LICENSES = [
     { code: "BJC-C04C2664", expire: "2026-01-07", device: null },
-    { code: "BJC-26A80A55", expire: "2026-01-07", device: null },
-    { code: "BJC-CF43F904", expire: "2026-01-07", device: null }
+    { code: "BJC-26A80A55", expire: "2026-01-07", device: null }
 ];
