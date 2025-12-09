53c93e9e1e2cc3192a550e0dd07843bc2281aea9
bjchack <bjchack@gmail.com>
2025-12-09 18:14:17 +0800

    Update license.js

--- license.js
+++ license.js
@@ -1,9 +1,6 @@
 // license.js
-// Listahan ng valid codes at expiry date
 const LICENSES = [
     { code: "BJC-C04C2664", expire: "2026-01-07", device: null },
     { code: "BJC-26A80A55", expire: "2026-01-07", device: null },
-    { code: "BJC-CF43F904", expire: "2026-01-07", device: null },
-    { code: "BJC-88591DFE", expire: "2025-12-11", device: null },
-    { code: "BJC-4A843004", expire: "2026-01-07", device: null }
+    { code: "BJC-CF43F904", expire: "2026-01-07", device: null }
 ];
